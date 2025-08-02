5954884431a7cd6360f5f45bd11e2873c5153a35
rodri-iot <rodrigo.j.pinedo@gmail.com>
2025-08-02 00:04:48 -0300

    iniciando

--- esp32_heltec_v3/components/mqtt_client/mqtt_client.h
+++
@@ -1,6 +0,0 @@
-#ifndef MQTT_CLIENT_H
-#define MQTT_CLIENT_H
-
-void mqtt_start(void);
-
-#endif
--- esp32_heltec_v3/components/wifi_manager/wifi_manager.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFI_MANAGER_H
-#define WIFI_MANAGER_H
-
-#include "esp_err.h"
-
-esp_err_t wifi_connect(void);
-
-#endif
